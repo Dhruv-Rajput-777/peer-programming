ea1c1a3127bd20ec6797dbb4b6e8e60061a30c67
Dhruv Rajput <raj.dhruv.777@gmail.com>
2023-01-07 21:45:05 +0530

    changed server url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pear-programming-server.herokuapp.com/",
+  baseURL: "https://pear-programming-server.cyclic.app/",
+  // baseURL: "https://pear-programming-server.herokuapp.com/",
   // baseURL: "http://localhost:8000/",
 });
