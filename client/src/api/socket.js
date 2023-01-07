c3ee3f74d61f4f67322887c874528a6a7c1ad431
Dhruv Rajput <raj.dhruv.777@gmail.com>
2023-01-07 22:33:59 +0530

    updated server url

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const serverURL = "https://pear-programming-server.herokuapp.com/";
+const serverURL = "https://pear-programming-server.cyclic.app/";
 const socket = io(serverURL);
 
 export { socket };
